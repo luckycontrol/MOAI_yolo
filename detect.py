f1cdcfd0ea98648a799b098b93dcf9853504b7ec
luckycontrol <beansu1@naver.com>
2025-01-03 16:37:26 +0900

    save-txt 수정

--- detect.py
+++ detect.py
@@ -438,7 +438,7 @@ def parse_opt():
     parser.add_argument("--imgsz", "--img", "--img-size", nargs="+", type=int, default=[640], help="inference size h,w")
     parser.add_argument("--conf-thres", type=float, default=0.25, help="confidence threshold")
     parser.add_argument("--device", default="0" if torch.cuda.is_available() else "cpu", help="cuda device, i.e. 0 or 0,1,2,3 or cpu")
-    parser.add_argument("--save-txt", type=bool, default=True, help="save results to *.txt")
+    parser.add_argument("--save-txt", action="store_true", help="save results to *.txt")
 
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
